6c8ca6a7a3d57e3f997653d22e5380de3b5ffec7
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2022-02-13 18:09:16 -0300

    fix

---
+++ telas-compartilhadas/registro de atividade/script.js
@@ -0,0 +1,16 @@
+document.querySelector('.innerHTML').innerHTML = `
+<div class="main2">
+    <div class="titulos">
+        <h3 class="h3_1">Hora extra <span>50%</span> </h3>
+        <h3 class="h3_2">Hora extra<span>100%</span></h3>
+    </div>
+    <ul class="lista_funcionarios">
+        <input class="checkbox"type="checkbox">
+        <h2 class="nome_funcionario">Funcionário</h2>
+        <input type="text" placeholder="Hora extra 50%">
+        <input type="text" placeholder="Hora extra 100%">
+    </ul> 
+</div>
+
+
+`

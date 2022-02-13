0f5c8ffb9345204ccac4fd4addfa222a2c301a13
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2022-02-13 18:41:55 -0300

    fix

--- sript.js
+++ sript.js
@@ -6,15 +6,15 @@ botao.addEventListener('click', e => {
     const login = document.querySelector('.inputLogin')
     const senha = document.querySelector('.inputSenha')
     const botao = document.querySelector('.botao')
-    if (login.value === 'lider' && senha.value === '123') {
+    if (login.value.toLowerCase() === 'lider' && senha.value === '123') {
         botao.setAttribute('href', './lider/index.html')
         login = ''
         senha = ''
-    } else if (login.value === 'administrador' && senha.value === '123') {
+    } else if (login.value.toLowerCase() === 'administrador' && senha.value === '123') {
         botao.setAttribute('href', './administrador/index.html')
         login = ''
         senha = ''
-    } else if (login.value === 'apontador' && senha.value === '123') {
+    } else if (login.value.toLowerCase() === 'apontador' && senha.value === '123') {
         botao.setAttribute('href', './apontador-fiscal/index.html')
         login = ''
         senha = ''

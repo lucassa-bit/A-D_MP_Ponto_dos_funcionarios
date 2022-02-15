3f9f683047cb6aa1e8066dd575b875b00def8d5c
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2022-02-15 20:45:14 -0300

    fix

--- telas-compartilhadas/funcionarios/script.js
+++ telas-compartilhadas/funcionarios/script.js
@@ -4,6 +4,17 @@ cadastrarBTN.addEventListener('click', e => {
     cadastrarBTN.setAttribute('href', './cadastrar/index.html')
 })
 
+function deleteById (id){
+    fetch('https://flash-point-app.herokuapp.com/api/funcionario/edit/delete?id=' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        },
+    }).then(response => response.json())
+    
+}
+
 fetch('https://flash-point-app.herokuapp.com/api/funcionario/findAll', {
     method: 'Get',
 }).then(response => response.json()).then(usuarios => {
@@ -12,29 +23,16 @@ fetch('https://flash-point-app.herokuapp.com/api/funcionario/findAll', {
         <div class="containerFuncionarios">
             <ul class="lista_funcionarios">
               <li class="funcionario" > Nome: ` + val.nome + ` - Cargo: ` + val.cargo +  `</li> 
-              <input class="deletarInput" type="submit" value="Deletar" /> 
+              <input class="deletarInput" type="submit" value="Deletar" onclick="deleteById(` +val.id+ `)"/> 
             </ul> 
         </div>    
-        `
+
+        `      
+
     })
-    const deletar = document.querySelectorAll('.deletarInput')
-    const funcionario = document.querySelectorAll('.funcionario')
-    console.log(funcionario[1])
+  
 })
 
-const deletar = document.querySelectorAll('.deletarInput')
-const funcionario = document.querySelectorAll('.funcionario')
 
 
-for (var i = 0; i < funcionario.length; i++) {
-    
-   /*  deletar[i].addEventListener('click', e =>{
-        fetch('https://flash-point-app.herokuapp.com/edit/delete/', {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem("token")
-            },
-        }).then(response => response.json())
-    })    */
-  } 
+

74e17e051a2e87f1714314325e33c50744f5461e
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2022-02-15 10:38:18 -0300

    fix

--- telas-compartilhadas/registro de atividade/script.js
+++ telas-compartilhadas/registro de atividade/script.js
@@ -6,9 +6,10 @@ data.addEventListener('change', e =>{
     fetch('https://flash-point-app.herokuapp.com/api/ponto/findByDate?data=' + teste ,{
     method: 'Get',
     headers: {'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBZG1pbiIsImV4cCI6MTY0NDkzMTg5Mn0.X0-goPFj-5_siI3hyQVak4-rMiUmojBxtAaXV2cefRHajXCY6CFXic4x0EfnNjFx9buv5bRuh2wT6GdpvOixlA'}
-    }).then(response => response.json()).then(funcionarios => {  
+    }).then(async (response => response.json())).then(funcionarios => {  
         document.querySelector('.innerHTML').innerHTML = ''      
         funcionarios.map((val) => {
+          /*   console.log(val.nome) */
             document.querySelector('.innerHTML').innerHTML += `
             <div class="main2">
                 <div class="titulos">
@@ -18,11 +19,16 @@ data.addEventListener('change', e =>{
                 <ul class="lista_funcionarios">
                     <input class="checkbox"type="checkbox">
                     <h2 class="nome_funcionario">` + val.empregado.nome + `</h2>
-                    <input type="number" placeholder="Hora extra 50%">
-                    <input type="number" placeholder="Hora extra 100%">
+                    <input class="horaExtra50" type="number" placeholder="Hora extra 50%">
+                    <input class="horaExtra100" type="number" placeholder="Hora extra 100%">
                 </ul> 
             </div>  `        
         }) 
+/* 
+        const horaExtra50 = document.querySelector('.horaExtra50').value
+        const horaExtra100 = document.querySelector('.horaExtra100').value */
+
+
        
     }) 
 })

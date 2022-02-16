0d884bc99a83dc9066d89c0d940e1d4238c1e569
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2022-02-15 23:56:52 -0300

    fix

--- telas-compartilhadas/registro de atividade/script.js
+++ telas-compartilhadas/registro de atividade/script.js
@@ -17,36 +17,38 @@ data.addEventListener('change', e =>{
                     <h3 class="h3_2">Hora extra<span>100%</span></h3>
                 </div>
                 <ul class="lista_funcionarios">
-                    <input class="checkbox"type="checkbox">
-                    <div style="cursor:pointer;" class="nome_funcionario">` + val.empregado.nome + `</div>
                     <input class="horaExtra50" type="number" placeholder="Hora extra 50%">
                     <input class="horaExtra100" type="number" placeholder="Hora extra 100%">
                 </ul> 
-                <div class="informacoesExtras" style="display: block;">
-                    <h3>Nome: ` + val.empregado.nome +`</h3>
-                    <h3>Cargo: ` + val.empregado.cargo +`</h3>
-                    <h3>Vínculo: ` + val.empregado.vinculo +`</h3>
-                    <h3>Valor: ` + val.empregado.valor +`</h3>
-                    <h3>RG: ` + val.empregado.rg +`</h3>
-                    <h3>CPF: ` + val.empregado.cpf +`</h3>
-                    <h3>Celular: ` + val.empregado.celular +`</h3>
-                    <h3>Pis: ` + val.empregado.pis +`</h3>
-                    <h3>Pagamento: ` + val.empregado.pagamento +`</h3>
-                </div>
+                <div style="cursor:pointer;" class="nome_funcionario">  <input class="checkbox"type="checkbox">` + val.empregado.nome + `
+
+                        <div class="informacoesExtras" style="display: none;">
+                            <h3>Nome: ` + val.empregado.nome +`</h3>
+                            <h3>Cargo: ` + val.empregado.cargo +`</h3>
+                            <h3>Vínculo: ` + val.empregado.vinculo +`</h3>
+                            <h3>Valor: ` + val.empregado.valor +`</h3>
+                            <h3>RG: ` + val.empregado.rg +`</h3>
+                            <h3>CPF: ` + val.empregado.cpf +`</h3>
+                            <h3>Celular: ` + val.empregado.celular +`</h3>
+                            <h3>Pis: ` + val.empregado.pis +`</h3>
+                            <h3>Pagamento: ` + val.empregado.pagamento +`</h3>
+                        </div>
+                    
+                    </div>
             </div>  `   
         }) 
 
-        const nomesFuncionarios = document.querySelectorAll('.nome_funcionario')
+        var nomesFuncionarios = document.querySelectorAll('.nome_funcionario')
         for (var i = 0; i < nomesFuncionarios.length; i++) {
           console.log(nomesFuncionarios.length)
           nomesFuncionarios[i].addEventListener('click', e =>{
-              if (e.target.querySelector('.informacoesExtras').style.display == 'block'){
-                  e.target.querySelector('.informacoesExtras').style.display == 'none'
+              if (e.target.querySelector('.informacoesExtras').style.display == 'none'){
+                  e.target.querySelector('.informacoesExtras').style.display = 'block'
               }else {
-                  e.target.querySelector('.informacoesExtras').style.display == 'block'
+                  e.target.querySelector('.informacoesExtras').style.display = 'none'
               }
           })   
-        } 
+        } // for
     }) 
  
 })

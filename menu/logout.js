0c862aa68017c4bb7658cf5f73d3390eb8c08b06
Lucas Almeida <lucasgsalmeida19@gmail.com>
2022-02-27 12:53:48 -0300

    adding relative paths

--- menu/logout.js
+++ menu/logout.js
@@ -2,5 +2,5 @@ const logout = document.querySelector("#logout");
 
 logout.addEventListener('click', (data) => {
     localStorage.removeItem("token");
-    window.location.href = '../index.html';
+    window.location.href = './../';
 });
